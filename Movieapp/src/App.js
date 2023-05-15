a98478b60d68be1c2a4951485b685600e49adf2b
jusish <ishimwejustin67@gmail.com>
2023-05-15 11:32:35 +0300

    initial commit

--- Movieapp/src/App.js
+++ Movieapp/src/App.js
@@ -31,7 +31,7 @@ const App  = () => {
     }, []);
     return (
         <div className='app'>
-            <h1>MovieLand</h1>
+            <h1>MovieLight</h1>
 
             <div className='search'>
                 <input
