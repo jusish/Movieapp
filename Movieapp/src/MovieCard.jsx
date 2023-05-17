4e553302588c0d7b048767cfc67f89051c2a14a0
jusish <ishimwejustin67@gmail.com>
2023-05-17 17:11:08 +0300

    initial commit

--- Movieapp/src/MovieCard.jsx
+++ Movieapp/src/MovieCard.jsx
@@ -1,21 +1,54 @@
-import React from "react";
+import { useState } from 'react';
 
-const MoiveCard = ({movie}) => {
-    return (
-        <div className='movie'>
-            <div>
-                <p>{movie.Year}</p>
-            </div>
-            <div>
-                <img src={movie.Poster !== 'https://m.media-amazon.com/images/M/MV5BYzYzZDViNWYtNWViMS00NDMxLThlN2YtZjFkOWMwODkzNzhiXkEyXkFqcGdeQXVyMTUwMzM4NzU0._V1_SX300.jpg' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.title} />
-            </div>
-    
-            <div>
-                <span>{movie.Type}</span>
-                <h3>{movie.Title}</h3>
-            </div>
-        </div>
-    );
-}
+const MovieCard = ({ movie }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-export default MoiveCard;
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="movie">
+      <div>
+        <p>{movie.Year}</p>
+      </div>
+      <div>
+        <img
+          src={
+            movie.Poster !==
+            'https://m.media-amazon.com/images/M/MV5BYzYzZDViNWYtNWViMS00NDMxLThlN2YtZjFkOWMwODkzNzhiXkEyXkFqcGdeQXVyMTUwMzM4NzU0._V1_SX300.jpg'
+              ? movie.Poster
+              : 'https://via.placeholder.com/400'
+          }
+          alt={movie.title}
+        />
+      </div>
+
+      <div>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
+        <button onClick={openModal} className="open">
+          Watch Trailer
+        </button>
+      </div>
+      <div className="model">
+        {isOpen && (
+          <div>
+            <button onClick={closeModal} className="close">
+              Close
+            </button>
+            <div>
+              <h1>{movie.title}</h1>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
--- README.md
+++
@@ -1,2 +0,0 @@
-# Movieapp
-This is the API generated app with react where you can get movies.
